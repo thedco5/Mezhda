6da812bd355d6b70b3f1d16feab29e6fa1bd6e4f
thedco5 <jarousqui@icloud.com>
2023-02-22 14:35:01 +0200

    .

--- Chatter.java
+++ Chatter.java
@@ -12,7 +12,6 @@ import javax.swing.AbstractAction;
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextPane;
 import javax.swing.JTextField;
--- Form.java
+++ Form.java
@@ -6,7 +6,6 @@ import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 import javax.swing.border.TitledBorder;
