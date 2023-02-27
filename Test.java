656c06fbe2b0c0905d196fc8ba36eb1908a84bf5
thedco5 <jarousqui@icloud.com>
2023-02-27 08:53:16 +0200

    .

--- Test.java
+++
@@ -1,9 +0,0 @@
-import util.Password;
-
-public class Test {
-    public static void main(String[] args) {
-        System.out.println(Password.encode("password"));
-        System.out.println(Password.encode("password"));
-        System.out.println(Password.encode("password").length());
-    } 
-}
