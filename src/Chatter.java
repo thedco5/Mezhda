f12b2d94334a5014d2ad3954c7e2e1ced9d4fa9e
thedco5 <jarousqui@icloud.com>
2023-03-09 15:09:25 +0200

    .

--- src/Chatter.java
+++ src/Chatter.java
@@ -69,7 +69,7 @@ public class Chatter {
     public static void main(String[] args) {
 
         /* CUSTOM FONT */
-        try {
+        try { 
             GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
             ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, new File("fonts/Lexend.ttf")));
             font = new Font("Lexend", Font.PLAIN, 20);
